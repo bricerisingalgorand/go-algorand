a65d7eb24e7182e1d35d521a532784db47e8f687
Brice Rising <brice@algorand.com>
2020-12-22 12:56:40 -0500

    See if computed branch channel is breaking master

--- scripts/compute_branch_channel.sh
+++ scripts/compute_branch_channel.sh
@@ -2,15 +2,5 @@
 
 # If workspace isn't clean, it's 'dev'.
 
-if [ "$1" = "master" ]; then
-    echo "master"
-elif [ "$1" = "rel/nightly" ]; then
-    echo "nightly"
-elif [ "$1" = "rel/stable" ]; then
-    echo "stable"
-elif [ "$1" = "rel/beta" ]; then
-    echo "beta"
-else
-    echo "dev"
-fi
+echo "master"
 
